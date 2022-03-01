823fbcf42219d905efc61a5266c703ec90fe043f
Shobha Manapati <smanapati@neiu.edu>
2022-02-28 22:27:56 -0600

    Homework submission 3.

--- GenericsAndCollections/Generics/src/Bag.java
+++ GenericsAndCollections/Generics/src/Bag.java
@@ -1,37 +1,59 @@
+// imports
+
 import java.util.ArrayList;
-
+import java.util.Random;
+// The `Bag` class
 public class Bag<T> {
+    // holds the underlying elements
     ArrayList<T> objects;
-    public Bag(){
-        /* TODO 
-Initialize the objects variable*/
-    }
-    /* TODO 
-code the add method described. It should be public and not 
-return anything. Just take in a parameter and  throw an 
-exception. Read the documentation for this.*/
 
- /* TODO 
-Code the drawOne method here. It should not take any
-parameters. It should return a generic object. More on the
-task description.*/
-
-    public /* return type?*/ drawMany(int howMany){
-/* TODO
-howMany is the number of elements to draw. 
-Create an ArrayList of the generic type and "drawOne" as
-many times as you need, putting the element drawn in the ArrayList.
-Then return it. */
+    // The `constructor`
+    public Bag() {
+        // initialize the ArrayList
+        this.objects = new ArrayList<>();
     }
 
-    public void add(T element,double prob) throws BagException{
-        /* TODO 
-Check that the probability is between 0 and 1. Otherwise
-throw an exception. */        // This is a way to add elements in a given proportion.
+    // Adds the `obj` to Bag
+    // Throws `BagException` if obj is null
+    public void add(T obj) throws BagException {
+        // check if the object is null
+        if (obj == null)    throw new BagException("Cannot add null to Bag");
+        // add the object to the end of list
+        objects.add(obj);
+    }
+
+    // Adds the `element` to Bag
+    // Throws `BagException` if ekement is null
+    public void add(T element, double prob) throws BagException {
+        // check if the object is null
+        if (element == null)    throw new BagException("Cannot add null to Bag");
+        // check if the probability is invalid
+        if (prob < 0 || prob > 1)   throw new BagException("Probability should be between 0 and 1");
+        // look at number of elements in `Bag`
+        // and insert new element with given probability
         int tot = (int)Math.round(objects.size()*prob);
-        for(int i=0;i<tot;i++){
-            System.out.println("Adding "+i);
+        for(int i=0; i < tot; i++){
             objects.add(element);
         }
     }
+
+    // The `drawOne` Method
+    public T drawOne() {
+        // get a random Index
+        int index = new Random().nextInt(this.objects.size());
+        // return the element at given index
+        return this.objects.get(index);
+    }
+
+    // The `drawMany` method
+    public ArrayList<T> drawMany(int howMany) {
+        // stores the elements to be returned
+        ArrayList<T> items = new ArrayList<>();
+        // draw elements at random
+        for (int i = 0; i < howMany; i++) items.add(drawOne());
+        // return the elements
+        return items;
+    }
+
+
 }
--- GenericsAndCollections/Generics/src/BingoChip.java
+++ GenericsAndCollections/Generics/src/BingoChip.java
@@ -1,4 +1,25 @@
-public class BingoChip/* TODO 
-Implement this class.
-Use the instructions and check how it is used in Task.java*/
+
+// The `BingoChip` class
+public class BingoChip<T1, T2> {
+
+    // The two generic variables of the class
+    private T1 item1;
+    private T2 item2;
+
+    // The `constructor`
+    public BingoChip(T1 item1, T2 item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    // Override the toString( )
+    @Override
+    public String toString() {
+        // return the string representation of the chip
+        return "Chip:" + item1 + "-" + item2;
+    }
+}
+
+
+
 
